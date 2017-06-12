4ab6b2585bc5ef13d2bfef18c1d780e734c858ec
AvatarSD <sd1995@ukr.net>
2017-06-12 10:27:19 +0300

    ads reading impl - not tested

--- Inc/ads1232.h
+++ Inc/ads1232.h
@@ -2,25 +2,23 @@
 #define ADS1232
 
 #include "stm32f1xx_hal.h"
+#include "stdbool.h"
 
-typedef struct _hx711
+typedef struct
 {
     GPIO_TypeDef* gpioSck;
     GPIO_TypeDef* gpioData;
     uint16_t pinSck;
     uint16_t pinData;
-    int offset;
-    /*int gain;
-    // 1: channel A, gain factor 128
-    // 2: channel B, gain factor 32
-    // 3: channel A, gain factor 64*/
+    int32_t offset;
+    bool needCalib;
 } ADS123x;
 
 
-void ADS_Init(ADS123x data);
-ADS123x ADS_Tare(ADS123x data, uint8_t times);
-int ADS_Value(ADS123x data);
-int ADS_AverageValue(ADS123x data, uint8_t times);
+void ADS_Init(const ADS123x *data);
+ADS123x *ADS_Tare(ADS123x *data, uint8_t times);
+int32_t ADS_Value(ADS123x *data);
+int32_t ADS_AverageValue(ADS123x *data, uint8_t times);
 
 
 
--- Src/ads1232.c
+++ Src/ads1232.c
@@ -1,45 +1,53 @@
 #include "ads1232.h"
 
-
-
-void ADS_Init(ADS123x data)
+void ADS_Init(const ADS123x * data)
 {
     GPIO_InitTypeDef GPIO_InitStruct;
-    GPIO_InitStruct.Pin = data.pinSck;
+    GPIO_InitStruct.Pin = data->pinSck;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(data.gpioSck, &GPIO_InitStruct);
+    HAL_GPIO_Init(data->gpioSck, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = data.pinData;
+    GPIO_InitStruct.Pin = data->pinData;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
     GPIO_InitStruct.Pull = GPIO_PULLUP;
-    HAL_GPIO_Init(data.gpioData, &GPIO_InitStruct);
-
-    /*HAL_GPIO_WritePin(data.gpioSck, data.pinSck, GPIO_PIN_SET);
-    HAL_Delay(50);
-    HAL_GPIO_WritePin(data.gpioData, data.pinSck, GPIO_PIN_RESET);*/
-}
-
-int ADS_Value(ADS123x data)
-{
+    HAL_GPIO_Init(data->gpioData, &GPIO_InitStruct);
 
 }
 
-int ADS_AverageValue(ADS123x data, uint8_t times)
+int32_t ADS_Value(ADS123x *data)
 {
-    int sum = 0;
-    for (int i = 0; i < times; i++)
+    int32_t adsVal = 0;
+    while (HAL_GPIO_ReadPin(data->gpioData, data->pinData));
+    for (uint8_t i = 0; i < 24; i++)
     {
-        sum += ADS_Value(data);
+        HAL_GPIO_WritePin(data->gpioSck, data->pinSck, GPIO_PIN_SET);
+        adsVal = adsVal << 1 ;
+        if (HAL_GPIO_ReadPin(data->gpioData, data->pinData)) adsVal ++;
+        HAL_GPIO_WritePin(data->gpioSck, data->pinSck, GPIO_PIN_RESET);
     }
+    if(data->needCalib)
+        for(uint8_t i = 0; i < 4; i++)
+            HAL_GPIO_TogglePin(data->gpioData, data->pinData);
+    data->needCalib = false;
 
+    //todo
+    //adsVal = adsVal ^ 0x800000;
+
+    return adsVal;
+}
+
+int32_t ADS_AverageValue(ADS123x * data, uint8_t times)
+{
+    int32_t sum = 0;
+    for (int i = 0; i < times; i++)
+        sum += ADS_Value(data);
     return sum / times;
 }
 
-ADS123x ADS_Tare(ADS123x data, uint8_t times)
+ADS123x *ADS_Tare(ADS123x *data, uint8_t times)
 {
-    int sum = ADS_AverageValue(data, times);
-    data.offset = sum;
+    data->offset = ADS_AverageValue(data, times);
     return data;
 }
 
